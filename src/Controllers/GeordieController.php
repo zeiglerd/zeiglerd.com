bd348d8d65f202f112365a53b38cfa8fc9d14110
Dustin Zeigler <dustin.zeigler@live.com>
2024-05-13 17:11:56 -0500

    bootstrap geordie

--- src/Controllers/GeordieController.php
+++ src/Controllers/GeordieController.php
@@ -5,5 +5,6 @@ namespace ZeiglerD\ZeiglerD\Controllers;
 final class GeordieController extends \ZeiglerD\PhpEngine\Controller
 {
   final public function index($Router, $Template) {
+    $this->addScripts('/scripts/templates/%s/views/geordie/geordie.min.js');
   }
 }
