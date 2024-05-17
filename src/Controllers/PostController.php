9eedef958d43f72cd1931aad27bdabfbc0901b19
Dustin Zeigler <dustin.zeigler@live.com>
2024-05-16 21:03:36 -0500

    boostrap post/resume
    * remove DEFAULT_THEME
    * cleanup content selection

---
+++ src/Controllers/PostController.php
@@ -0,0 +1,9 @@
+<?php // declare(strict_types=1);
+
+namespace ZeiglerD\ZeiglerD\Controllers;
+
+final class PostController extends \ZeiglerD\PhpEngine\Controllers\PostController
+{
+  public function resume($Router, $Template) {
+  }
+}
