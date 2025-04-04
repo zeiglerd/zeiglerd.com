cda1565fe55cabb783f1dd7db0663ead1d1e7fbc
Dustin Zeigler <dustin.zeigler@live.com>
2025-04-04 03:21:05 -0500

    Update RouterTest.php

--- tests/RouterTest.php
+++ tests/RouterTest.php
@@ -1,6 +1,6 @@
 <?php declare(strict_types=1);
 
-namespace ZeiglerD\PhpEngine\UnitTests;
+namespace ZeiglerD\ZeiglerD\UnitTests;
 
 use ZeiglerD\PhpEngine\UnitTests\Helpers\QueryableHtml;
 use ZeiglerD\PhpEngine\UnitTests\Helpers\TestCase;
